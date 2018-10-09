e61604ef01ca023a298a0ab8981990797706d2d8
孙晓磊 <sunxl8@centaline.com.cn>
2018-10-09 16:02:08 +0800

    create

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new Center(
+      child: new Text(
+        'Hello Flutter',
+        textDirection: TextDirection.ltr,
+      ),
+    ));
